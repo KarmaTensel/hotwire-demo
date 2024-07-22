4ada778fee974085aa3b3bdf4f17aee0c9f9d858
Karma Tensel <42111043+KarmaTensel@users.noreply.github.com>
2024-07-22 14:42:42 +0600

    chore: stimulus demo

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :tasks
+  resources :tasks do
+    member do
+      post :toggle
+    end
+  end
 end
